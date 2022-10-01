4f49341df1ceaa71bf8f3b9648a1997f09d395f2
KIIZA STEPHEN <53976517+Atwiinesteve@users.noreply.github.com>
2022-10-01 10:48:34 +0000

    Added Circular Queue Data Structure

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ console.log(stack.peek())
 console.log('\n')
 
 // QUEUE IN JAVASCRIPT
-console.log('===== QUEUE IN JAVASCRIPT (LIFO Principle) ===== ');
+console.log('===== QUEUE IN JAVASCRIPT (FIFO Principle) ===== ');
 class Queue1 { // O(n) - Linear Time Complexity
   constructor() {
     this.items = [];
@@ -183,3 +183,76 @@ queue2.enqueue(15)
 queue2.print()
 console.log(queue2.dequeue())
 console.log(queue2.peek());
+
+
+            
+// =========================
+console.log('\n');
+
+// CIRCULAR QUEUE IN JAVASCRIPT
+console.log('===== CIRCULAR QUEUE IN JAVASCRIPT (FIFO Principle) ===== ');
+class CircularQueue {
+  constructor(capacity) {
+    this.items = new Array(capacity);
+    this.capacity = capacity;
+    this.currentLength = 0;
+    this.back = -1;
+    this.front = -1;
+  }
+  isFull() {
+    return this.currentLength === this.capacity;
+  }
+  isEmpty() {
+    return this.currentLength === 0;
+  }
+  enqueue(element) {
+    if(!this.isFull()) {
+      this.items[this.back] = element;
+      this.back = (this.back + 1) % this.capacity;
+      this.currentLength += 1;
+      if(this.front === -1) {
+        this.back = this.front;
+      }
+    }
+  }
+  dequeue() {
+    if(!this.isEmpty) return null;
+    const item = this.items[this.front];
+    this.items[this.front] = null;
+    this.front = (this.front + 1) % this.capacity
+    this.currentLength -= 1;
+    if(!this.isEmpty()) {
+      this.front = -1;
+      this.back = -1;
+    }
+    return item;
+  }
+  peek() {
+    if(!this.isEmpty()) {
+      return this.items[this.front]
+    }
+    return null;
+  }
+  print() {
+    if(this.isEmpty()) {
+      consol.log('Queue Empty')
+    } else {
+      let i;
+      let str = '';
+      for(i = this.front; i !== this.back; i = (i + 1) % this.capacity) {
+        str += this.items[i] + ' '
+      }
+      str += this.items[i];
+      console.log(str);
+    }
+  }
+};
+
+const cicularQueue = new CircularQueue();
+console.log(cicularQueue.isEmpty())
+cicularQueue.enqueue(12)
+cicularQueue.enqueue(13)
+cicularQueue.enqueue(14)
+cicularQueue.enqueue(15)
+cicularQueue.enqueue(16)
+console.log(cicularQueue.isEmpty())
